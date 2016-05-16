6987fcd1c58596802cb1d63a0b5d568aad1019d5
rsafeup <renatoabreu__@hotmail.com>
2016-05-16 17:38:52 +0100

    drone movement changes

--- TP6/Ex 5/MyDroneHandler.js
+++ TP6/Ex 5/MyDroneHandler.js
@@ -13,13 +13,37 @@
  	this.angle = 0;
 
 	this.angleStep = 5;
-	this.velocity = 0;
-	this.upwardVelocity = 0;
-	this.acceleration = 0.02;
-	this.friction = 0.7;
+	this.motionVelocity = 0;
+	this.floatVelocity = 0;
+	this.acceleration = 0.8;
+	this.friction = 0.4;
+	this.maxVelocity = Math.sqrt(this.acceleration/this.friction);
+ 	this.maxTime = 2.5/(this.maxVelocity);
  	this.slowRotation = 0.2 * 360;
  	this.normalRotation = 1 * 360;
  	this.fastRotation = 10 * 360;
+
+ 	this.motionTime = 0;
+ 	this.motionTimeStart = 0;
+ 	this.floatTime = 0;
+ 	this.floatTimeStart = 0;
+
+ 	this.motionTrajectory ={
+ 	      Forward:1,
+ 	      Backward:2,
+ 	      Halting:3,
+ 	      Stopped:4
+ 	}
+
+ 	this.floatTrajectory ={
+ 	      Upward:1,
+ 	      Downward:2,
+ 	      Halting:3,
+ 	      Stopped:4
+ 	}
+
+ 	this.motionState = this.motionTrajectory.Stopped;
+ 	this.floatState = this.floatTrajectory.Stopped;
 
  	//Animation related methods
 	this.initialTime = 0;	// Initial time in miliseconds
@@ -40,13 +64,23 @@ MyDroneHandler.prototype.setRotation = function(orientation){
 }
 
 MyDroneHandler.prototype.move = function(orientation){
-	this.velocity = Math.sqrt(this.acceleration/this.friction) * (Math.exp(2*Math.sqrt(this.acceleration*this.friction) * this.elapsedTime)- 1)/(Math.exp(2*Math.sqrt(this.acceleration*this.friction) * this.elapsedTime) + 1);
-	this.velocity *= orientation;
+	if(this.motionState == this.motionTrajectory.Stopped){
+		if(this.orientation == 1){
+			this.motionState = this.motionTrajectory.Forward;
+		} else this.motionState = this.motionTrajectory.Backward;
+		this.motionTime = 0;
+		this.motionTimeStart = this.elapsedTime;
+	}
 }
 
 MyDroneHandler.prototype.fly = function(orientation){
-	this.upwardVelocity = Math.sqrt(this.acceleration/this.friction) * (Math.exp(2*Math.sqrt(this.acceleration*this.friction) * this.elapsedTime)- 1)/(Math.exp(2*Math.sqrt(this.acceleration*this.friction) * this.elapsedTime) + 1);
-	this.upwardVelocity *= orientation;
+	if(this.floatState == this.floatTrajectory.Stopped){
+		if(this.orientation == 1){
+			this.floatState = this.floatTrajectory.Upward;
+		} else this.floatState = this.floatTrajectory.Downward;
+		this.floatTime = 0;
+		this.floatTimeStart = this.elapsedTime;
+	}
 }
 
 MyDroneHandler.prototype.update = function(currTime){
@@ -59,13 +93,41 @@ MyDroneHandler.prototype.update = function(currTime){
 		this.elapsedTime = (currTime - this.initialTime)/1000;
 		var rotation = this.normalRotation * this.elapsedTime;
 		this.drone.setHelicesRotation(rotation, rotation, rotation ,rotation);
+	
+		if(this.motionState == this.motionTrajectory.Forward){
+			this.motionTime = this.elapsedTime - this.motionTimeStart;
+			this.motionVelocity = (this.maxVelocity * (Math.exp(2* this.maxVelocity * this.motionTime)- 1))/(Math.exp(2* this.maxVelocity * this.motionTime) + 1);
 
-		this.X += this.velocity * Math.sin(this.angle*degToRad);
-		this.Z += this.velocity * Math.cos(this.angle*degToRad);
-		this.Y += this.upwardVelocity;
+			if(this.motionTime > this.maxTime){
+				this.motionState = this.motionTrajectory.Halting;
+			}
+		} else if(this.motionState == this.motionTrajectory.Backward){
 
-		this.velocity *= this.friction;
-		this.upwardVelocity *= this.friction;
+		} else if(this.motionState == this.motionTrajectory.Halting){
+
+		} else{
+			this.motionVelocity = 0;
+		}
+
+
+		if(this.floatState == this.floatTrajectory.Upward){
+			this.floatTime = this.elapsedTime - this.floatTimeStart;
+			this.floatVelocity = (this.maxVelocity * (Math.exp(2* this.maxVelocity * this.floatTime)- 1))/(Math.exp(2* this.maxVelocity * this.floatTime) + 1);
+			
+			if(this.floatTime > this.maxTime){
+				this.floatState = this.floatTrajectory.Halting;
+			}
+		} else if(this.floatState == this.floatTrajectory.Downward){
+
+		} else if(this.floatState == this.floatTrajectory.Halting){
+
+		} else{
+			this.floatVelocity = 0;
+		}
+
+		this.X += this.motionVelocity* 0.04 * Math.sin(this.angle*degToRad);
+		this.Z += this.motionVelocity* 0.04 * Math.cos(this.angle*degToRad);
+		this.Y += this.floatVelocity * 0.04;
 	}
  }
 
